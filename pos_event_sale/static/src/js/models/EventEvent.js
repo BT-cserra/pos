1de55a1c79ec60a2e034a6d4a617bee23a70f6c0
Carlos Serra-Toro <carlos.serra@braintec.com>
2023-06-16 09:56:54 +0200

    WiP 2

--- pos_event_sale/static/src/js/models/EventEvent.js
+++ pos_event_sale/static/src/js/models/EventEvent.js
@@ -7,81 +7,80 @@ odoo.define("pos_event_sale.EventEvent", function (require) {
     "use strict";
 
     const { getDatesInRange } = require("pos_event_sale.utils");
-    const EventEvent = require('pos_event_sale.EventEvent');
+    const PosComponent = require("point_of_sale.PosComponent");
     const Registries = require('point_of_sale.Registries');
 
-    const PosResEventEvent = EventEvent =>
-        class extends EventEvent {
-            initialize(attr, options) {
-                Object.assign(this, options);
-            }
 
-            getEventTickets() {
-                return this.db.getEventTicketsByEventID(this.id);
-            }
-
-            /**
-             * Computes the total ordered quantity for this event.
-             *
-             * @param {Order} options.order defaults to the current order
-             * @returns {Number} ordered quantity
-             */
-            getOrderedQuantity({order} = {}) {
-                order = order ? order : this.env.pos.get_order();
-                if (!order) {
-                    return 0;
-                }
-                return order
-                    .get_orderlines()
-                    .filter((line) => line.getEvent() === this)
-                    .reduce((sum, line) => sum + line.quantity, 0);
-            }
-
-            /**
-             * Computes the available places, considering all the ordered quantities in
-             * the current order.
-             *
-             * Please note it doesn't check the available seats against the backend.
-             * For a real availability check see updateAndCheckEventAvailability.
-             *
-             * @param {Object} options - Sent to getOrderedQuantity
-             * @returns {Number} available seats
-             */
-            getSeatsAvailable(options) {
-                return this.seats_limited
-                    ? this.seats_available - this.getOrderedQuantity(options)
-                    : this.seats_available;
-            }
-
-            getTicketSeatsAvailable(options) {
-                return this.getEventTickets()
-                    .map((ticket) => ticket.getSeatsAvailable(options))
-                    .reduce((sum, qty) => sum + qty, 0);
-            }
-
-            /**
-             * Similar to getSeatsAvailable, but also checks its ticket's availability.
-             * It's useful to display the real availability in the UI, that accounts for
-             * both the event and the tickets availability.
-             *
-             * @param {Object} options - Sent to getOrderedQuantity
-             * @returns {Number} available seats
-             */
-            getSeatsAvailableReal(options) {
-                const ticketSeatsAvailable = this.getTicketSeatsAvailable(options);
-                const eventSeatsAvailable = this.getSeatsAvailable(options);
-                return Math.min(ticketSeatsAvailable, eventSeatsAvailable);
-            }
-
-            /**
-             * @returns {[Date]} List of Dates for which this event is available
-             */
-            getEventDates() {
-                return getDatesInRange(this.date_begin, this.date_end);
-            }
+    class EventEvent extends PosComponent{
+        initialize(attr, options) {
+            Object.assign(this, options);
         }
 
-    Registries.Component.extend(EventEvent, PosResEventEvent);
+        getEventTickets() {
+            return this.db.getEventTicketsByEventID(this.id);
+        }
 
+        /**
+         * Computes the total ordered quantity for this event.
+         *
+         * @param {Order} options.order defaults to the current order
+         * @returns {Number} ordered quantity
+         */
+        getOrderedQuantity({order} = {}) {
+            order = order ? order : this.env.pos.get_order();
+            if (!order) {
+                return 0;
+            }
+            return order
+                .get_orderlines()
+                .filter((line) => line.getEvent() === this)
+                .reduce((sum, line) => sum + line.quantity, 0);
+        }
+
+        /**
+         * Computes the available places, considering all the ordered quantities in
+         * the current order.
+         *
+         * Please note it doesn't check the available seats against the backend.
+         * For a real availability check see updateAndCheckEventAvailability.
+         *
+         * @param {Object} options - Sent to getOrderedQuantity
+         * @returns {Number} available seats
+         */
+        getSeatsAvailable(options) {
+            return this.seats_limited
+                ? this.seats_available - this.getOrderedQuantity(options)
+                : this.seats_available;
+        }
+
+        getTicketSeatsAvailable(options) {
+            return this.getEventTickets()
+                .map((ticket) => ticket.getSeatsAvailable(options))
+                .reduce((sum, qty) => sum + qty, 0);
+        }
+
+        /**
+         * Similar to getSeatsAvailable, but also checks its ticket's availability.
+         * It's useful to display the real availability in the UI, that accounts for
+         * both the event and the tickets availability.
+         *
+         * @param {Object} options - Sent to getOrderedQuantity
+         * @returns {Number} available seats
+         */
+        getSeatsAvailableReal(options) {
+            const ticketSeatsAvailable = this.getTicketSeatsAvailable(options);
+            const eventSeatsAvailable = this.getSeatsAvailable(options);
+            return Math.min(ticketSeatsAvailable, eventSeatsAvailable);
+        }
+
+        /**
+         * @returns {[Date]} List of Dates for which this event is available
+         */
+        getEventDates() {
+            return getDatesInRange(this.date_begin, this.date_end);
+        }
+    }
+
+    Registries.Component.add(EventEvent);
     return EventEvent;
 });
--- pos_event_sale/static/src/js/models/EventTicket.js
+++ pos_event_sale/static/src/js/models/EventTicket.js
@@ -6,85 +6,83 @@ License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 odoo.define("pos_event_sale.EventTicket", function (require) {
     "use strict";
 
-    const EventTicket = require('pos_event_sale.EventTicket');
+    const PosComponent = require("point_of_sale.PosComponent");
     const Registries = require('point_of_sale.Registries');
 
-    const PosResEventTicket = EventTicket =>
-        class extends EventTicket {
-            initialize(attr, options) {
-                Object.assign(this, options);
-            }
-
-            getEvent() {
-                return this.db.getEventByID(this.event_id[0]);
-            }
-
-            getProduct() {
-                return this.db.get_product_by_id(this.product_id[0]);
-            }
-
-            getPriceExtra() {
-                return this.price - this.getProduct().lst_price;
-            }
-
-            _prepareOrderlineOptions() {
-                return {
-                    price_extra: this.getPriceExtra(),
-                    extras: {
-                        event_ticket_id: this.id,
-                    },
-                };
-            }
-
-            /**
-             * Computes the total ordered quantity for this event ticket.
-             *
-             * @param {Object} options
-             * @param {Order} options.order defaults to the current order
-             * @returns {Number} ordered quantity
-             */
-            getOrderedQuantity({ order } = {}) {
-                order = order ? order : this.env.pos.get_order();
-                if (!order) {
-                    return 0;
-                }
-                return order
-                    .get_orderlines()
-                    .filter((line) => line.getEventTicket() === this)
-                    .reduce((sum, line) => sum + line.quantity, 0);
-            }
-
-            /**
-             * Computes the available places, considering all the ordered quantities in
-             * the current order.
-             *
-             * Please note it doesn't check the available seats against the backend.
-             * For a real availability check see updateAndCheckEventAvailability.
-             *
-             * @param {Object} options - Sent to getOrderedQuantity
-             * @returns {Number} available seats
-             */
-            getSeatsAvailable(options) {
-                return this.seats_limited
-                    ? this.seats_available - this.getOrderedQuantity(options)
-                    : this.seats_available;
-            }
-
-            /**
-             * Similar to getSeatsAvailable, but also checks the event's availability.
-             *
-             * @param {Object} options - Sent to getOrderedQuantity
-             * @returns {Number} available seats
-             */
-            getSeatsAvailableReal(options) {
-                const event = this.getEvent();
-                const ticketSeatsAvailable = this.getSeatsAvailable(options);
-                const eventSeatsAvailable = event.getSeatsAvailable(options);
-                return Math.min(ticketSeatsAvailable, eventSeatsAvailable);
-            }
+    class EventTicket extends PosComponent{
+        initialize(attr, options) {
+            Object.assign(this, options);
         }
 
-    Registries.Component.extend(EventTicket, PosResEventTicket);
+        getEvent() {
+            return this.db.getEventByID(this.event_id[0]);
+        }
 
+        getProduct() {
+            return this.db.get_product_by_id(this.product_id[0]);
+        }
+
+        getPriceExtra() {
+            return this.price - this.getProduct().lst_price;
+        }
+
+        _prepareOrderlineOptions() {
+            return {
+                price_extra: this.getPriceExtra(),
+                extras: {
+                    event_ticket_id: this.id,
+                },
+            };
+        }
+
+        /**
+         * Computes the total ordered quantity for this event ticket.
+         *
+         * @param {Object} options
+         * @param {Order} options.order defaults to the current order
+         * @returns {Number} ordered quantity
+         */
+        getOrderedQuantity({ order } = {}) {
+            order = order ? order : this.env.pos.get_order();
+            if (!order) {
+                return 0;
+            }
+            return order
+                .get_orderlines()
+                .filter((line) => line.getEventTicket() === this)
+                .reduce((sum, line) => sum + line.quantity, 0);
+        }
+
+        /**
+         * Computes the available places, considering all the ordered quantities in
+         * the current order.
+         *
+         * Please note it doesn't check the available seats against the backend.
+         * For a real availability check see updateAndCheckEventAvailability.
+         *
+         * @param {Object} options - Sent to getOrderedQuantity
+         * @returns {Number} available seats
+         */
+        getSeatsAvailable(options) {
+            return this.seats_limited
+                ? this.seats_available - this.getOrderedQuantity(options)
+                : this.seats_available;
+        }
+
+        /**
+         * Similar to getSeatsAvailable, but also checks the event's availability.
+         *
+         * @param {Object} options - Sent to getOrderedQuantity
+         * @returns {Number} available seats
+         */
+        getSeatsAvailableReal(options) {
+            const event = this.getEvent();
+            const ticketSeatsAvailable = this.getSeatsAvailable(options);
+            const eventSeatsAvailable = event.getSeatsAvailable(options);
+            return Math.min(ticketSeatsAvailable, eventSeatsAvailable);
+        }
+    }
+
+    Registries.Component.add(EventTicket);
     return EventTicket;
 });
